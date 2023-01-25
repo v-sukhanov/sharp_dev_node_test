c862bb1f9fb77c38f1dda1905a57da6a0c01437b
vladimirsuhanov <vladimir.sukhanov99@gmail.com>
2023-01-25 23:46:39 +0300

    adde env

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http')
 const fs = require('fs')
 const path = require('path')
 
+const port = process.env.PORT ?? 300;
+
 const server = http.createServer((request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
@@ -26,11 +28,11 @@ const server = http.createServer((request, response) => {
             response.end(data)
         }
     })
-    // response.end('<h1> Hello nodejs</h1>')
 })
 
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log('server has been started')
 })
+
 
